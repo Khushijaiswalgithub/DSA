389cc9c0fac37bca9c9b839077acb5467e308636
Khushi Jaiswal <159470469+Khushijaiswalgithub@users.noreply.github.com>
2025-04-12 11:48:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void dfs(vector<vector<int>>& image, int r, int c, int originalColor, int newColor) {
+        if (r < 0 || r >= image.size() || c < 0 || c >= image[0].size())
+            return;
+
+        if (image[r][c] != originalColor)
+            return;
+        image[r][c] = newColor;
+        dfs(image, r + 1, c, originalColor, newColor); 
+        dfs(image, r - 1, c, originalColor, newColor); 
+        dfs(image, r, c + 1, originalColor, newColor); 
+        dfs(image, r, c - 1, originalColor, newColor); 
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int newColor) {
+        // Code here
+        int originalColor = image[sr][sc];
+        if (originalColor != newColor)
+            dfs(image, sr, sc, originalColor, newColor);
+        return image;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> image(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cin >> image[i][j];
+        }
+        int sr, sc, newColor;
+        cin >> sr >> sc >> newColor;
+        Solution obj;
+        vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+        for (auto i : ans) {
+            for (auto j : i)
+                cout << j << " ";
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
