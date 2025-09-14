48b3c6826d8ef28de961dfd0f04c2f173d53765d
Khushi Jaiswal <159470469+Khushijaiswalgithub@users.noreply.github.com>
2025-09-14 08:19:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,71 +1,20 @@
-//{ Driver Code Starts
-// C++ program to find union of
-// two sorted arrays
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-/*You are required to complete this method*/
 class Solution {
   public:
     int startStation(vector<int> &gas, vector<int> &cost) {
-        // Your code here
-        int totalGas = 0, totalCost = 0, startIndex = 0, currentGas = 0;
-
-    for (int i = 0; i < gas.size(); i++) {
-        totalGas += gas[i];
-        totalCost += cost[i];
-        currentGas += gas[i] - cost[i];
-
-        if (currentGas < 0) {
-            startIndex = i + 1;
-            currentGas = 0;
+        //  code here
+        int n=gas.size();
+        int t=0;
+        int curr=0;
+        int s=0;
+        for(int i=0;i<n;i++){
+            t+=gas[i]-cost[i];
+            curr+=gas[i]-cost[i];
+            if(curr<0){
+                s=i+1;
+                curr=0;
+            }
+            
         }
-    }
-
-    return (totalGas >= totalCost) ? startIndex : -1;
-
-        
+        return (t>=0)?s:-1;
     }
 };
-
-
-
-//{ Driver Code Starts.
-/* Driver program to test above function */
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-
-    while (t--) {
-
-        vector<int> gas, cost;
-        string input1;
-        getline(cin, input1);
-        stringstream ss(input1);
-        int number1;
-        while (ss >> number1) {
-            gas.push_back(number1);
-        }
-        string input2;
-        getline(cin, input2);
-        stringstream sss(input2);
-        int number2;
-        while (sss >> number2) {
-            cost.push_back(number2);
-        }
-        Solution ob;
-        int ans = ob.startStation(gas, cost);
-
-        cout << ans << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
