b3db0cfb54122bd3b99e73244ecea68559f22d7c
Khushi Jaiswal <159470469+Khushijaiswalgithub@users.noreply.github.com>
2026-01-09 23:34:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays With At Most K Distinct Integers/subarrays-with-at-most-k-distinct-integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int countAtMostK(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        unordered_map<int, int> freq;
+        int left = 0;
+        long long count = 0;
+
+        for (int right = 0; right < n; right++) {
+            freq[arr[right]]++;
+
+            while (freq.size() > k) {
+                freq[arr[left]]--;
+                if (freq[arr[left]] == 0) {
+                    freq.erase(arr[left]);
+                }
+                left++;
+            }
+
+            count += (right - left + 1);
+        }
+
+        return count;
+    }
+};
